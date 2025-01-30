5325f0803905da6734b74383a7a678d008e06186
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-01-30 16:12:27 -0500

    Task 1

---
+++ in_class_assignment_1.js
@@ -0,0 +1,13 @@
+// Task 1
+
+let x = 10;
+if (x > 5) {
+    console.log("Help")
+};
+
+let expenses = 10000;
+if (expenses > 7000) {
+    console.log("High Expenses");
+} else {
+    console.log("Manageable Expenses");
+};
